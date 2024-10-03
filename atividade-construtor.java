fc6d8529f6c8531dd2e658e762870c3d2bb785bf
nonato-barroso91 <nonatobarroso14@gmail.com>
2024-10-03 20:19:05 -0300

    Create atividade-construtor.java

---
+++ atividade-construtor.java
@@ -0,0 +1,77 @@
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private String prazo;
+    private int prioridade;
+
+    public Tarefa(String titulo, String descricao, String prazo, int prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prazo = prazo;
+        setPrioridade(prioridade);
+    }
+
+    public Tarefa(String titulo, int prioridade) {
+        this.titulo = titulo;
+        this.descricao = "Aula";
+        this.prazo = "30/09/2024";
+        setPrioridade(prioridade);
+    }
+
+    private void setPrioridade(int prioridade) {
+        if (isPrioridadeValida(prioridade)) {
+            this.prioridade = prioridade;
+        } else {
+            throw new IllegalArgumentException("Prioridade inválida. Deve estar entre 1 e 5.");
+        }
+    }
+
+    private boolean isPrioridadeValida(int prioridade) {
+        return prioridade >= 1 && prioridade <= 5;
+    }
+
+    // Métodos Get e Set para os outros atributos
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Prazo: " + prazo);
+        System.out.println("Prioridade: " + prioridade);
+    }
+
+    public static void main(String[] args) {
+        // Testando os construtores
+        Tarefa tarefa1 = new Tarefa("Estudar para prova", "Revisar para Prova", "30/08/2024", 3);
+        tarefa1.exibirDetalhes();
+
+        Tarefa tarefa2 = new Tarefa("Estudar Java ", 2);
+        tarefa2.exibirDetalhes();
+    }
+}
